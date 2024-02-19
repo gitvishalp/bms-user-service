930e3318fb93d99b4f198182a0d1fb3ac1ded1c0
vishal-cqs <vishal@cqs.in>
2024-02-19 17:03:49 +0530

    initializing files

---
+++ src/main/java/com/bms/BmsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsUserServiceApplication.class, args);
+	}
+
+}
